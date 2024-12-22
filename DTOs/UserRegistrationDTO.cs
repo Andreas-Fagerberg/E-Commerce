16d0e280cba9f16df9ccd0c30486de98aa34cde7
LudwigStenberg <contact@ludwigstenberg.com>
2024-12-22 18:31:24 +0100

    Add User.cs for EF and begin first DTO

---
+++ DTOs/UserRegistrationDTO.cs
@@ -0,0 +1,10 @@
+namespace E_commerce_Databaser_i_ett_sammanhang;
+
+
+public class UserRegistrationDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ User.cs
@@ -0,0 +1,35 @@
+
+namespace E_commerce_Databaser_i_ett_sammanhang;
+
+public class User
+{
+    [Key]
+    public Guid UserId { get; set; }
+
+    [Required]
+    [MaxLength(75)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(75)]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress] // Validation
+    public string Email { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string PasswordHash { get; set; }
+
+    [Required]
+    public DateTime CreatedAt { get; set; }
+
+    // FK: One-to-Many (one user can have many orders)
+    public ICollection<Order> Orders { get; set; } // Can also be List<T>.
+
+    // FK: One-to-One (one user can have one cart)
+    // public Cart Cart { get; set; } -- waiting for Cart.cs
+
+    // Move this to dbContext file: public DbSet<User> Users { get; set; }
+}
