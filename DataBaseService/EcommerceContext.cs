aa0b672715ab5ac5aa953476ec156ce7f2cb6afd
alexanderluukkonens <alexanderluukkonens@hotmail.com>
2024-12-30 16:37:40 +0100

    Fix missing code

--- DataBaseService/EcommerceContext.cs
+++ DataBaseService/EcommerceContext.cs
@@ -8,24 +8,21 @@ public class EcommerceContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<Product> Products { get; set; }
-    public DbSet<ShoppingCart> Shopping_carts { get; set; } // Characters correct?
-    public DbSet<ProductOrders> Product_orders { get; set; } // Characters correct?
-    public DbSet<Address> Address { get; set; }
+    public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    public DbSet<ProductOrders> ProductOrders { get; set; }
+    public DbSet<Address> Addresses { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
         builder.UseNpgsql("Host=localhost;Database=ECommerce;Username=postgres;Password=password");
     }
 
-    /*
-   Om man vill konfigurera modellerna lite extra: fler constraints exempelvis, då kan man använda 'OnModelCreating'.
-   protected override void OnModelCreating(ModelBuilder builder)
-   {
-       builder.Entity<Todo>()
-           .Property(todo => todo.Title)
-           .IsRequired();
-   }
-   
-    //////////// Is this something we need/want to implement in our application ? ////////////////
-   */
+    //Om man vill konfigurera modellerna lite extra: fler constraints exempelvis, då kan man använda 'OnModelCreating'.
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder
+            .Entity<User>()
+            .Property(User => User.CreatedAt)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+    }
 }
