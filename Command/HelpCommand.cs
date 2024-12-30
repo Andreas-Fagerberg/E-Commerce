f71b63281e0086618df038cf29fffeb4aa8c947d
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2024-12-30 15:38:10 +0100

    Add: New commands to handle logic related to categories, and menus. Change: Menu display.

--- Command/HelpCommand.cs
+++ Command/HelpCommand.cs
@@ -1,12 +1,10 @@
-
-
 namespace E_commerce_Databaser_i_ett_sammanhang;
 
 public class HelpCommand : BaseCommand
 {
     public HelpCommand(ConsoleKey triggerKey, IUserService userService) : base(triggerKey, userService)
     {
-        
+
     }
 
     public override Task Execute(Guid? currentUserId)
