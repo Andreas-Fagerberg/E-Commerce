7fead18c8a5896d70b1401abc8f84d77b2e10c98
alexanderluukkonens <alexanderluukkonens@hotmail.com>
2024-12-30 15:42:14 +0100

    Add database accessibility

--- Command/ICommand.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace E_commerce_Databaser_i_ett_sammanhang;
-
-public interface ICommand
-{
-    ConsoleKey TriggerKey { get; }
-    Task Execute(Guid? currentUserId);
-}
--- Program.cs
+++ Program.cs
@@ -4,49 +4,8 @@ using Microsoft.EntityFrameworkCore;
 
 class Program
 {
-    static async void Main(string[] args)
+    static void Main(string[] args)
     {
-        IMenuService menuService = new AppMenuService();
-        while (true)
-        {
-            Console.Clear();
-            menuService.GetMenu().Display();
-            ConsoleKey input = Console.ReadKey().Key;
-
-            switch (input)
-            {
-                case ConsoleKey.Escape
-                or ConsoleKey.F7:
-                    Environment.Exit(0);
-                    break;
-                case ConsoleKey.LeftArrow
-                or ConsoleKey.A:
-
-                    continue;
-
-                case ConsoleKey.RightArrow
-                or ConsoleKey.D:
-
-                    continue;
-            }
-            foreach (ConsoleKey consoleKey in menuService.GetTriggerKeys())
-            {
-                if (input.Equals(consoleKey))
-                {
-                    menuService.ChangeMenu(input);
-                    continue;
-                }
-            }
-            try
-            {
-                await menuService.GetMenu().ExecuteCommand(input);
-            }
-            catch (Exception ex)
-            {
-                string message = string.IsNullOrEmpty(ex.Message)
-                    ? "Something went wrong, please try again."
-                    : ex.Message;
-            }
-        }
+        Console.WriteLine("Hello World!");
     }
 }
