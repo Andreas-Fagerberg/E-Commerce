a2e810916955f90001c12823b21140704d04cfa8
LudwigStenberg <contact@ludwigstenberg.com>
2025-01-06 14:03:11 +0100

    Create UpdateUserAddress()
    1. Validates user identity
    2. Checks if User has existing address. If not, throw.
    3. If user address exists, update the fields according to the DTO passed in.
    4. Save the changes to the database.
    5. Return AddressResponse object.

---
+++ Commands/UpdateUserAddressCommand.cs
@@ -0,0 +1,15 @@
+
+namespace E_commerce_Databaser_i_ett_sammanhang;
+
+public class UpdateUserAddressCommand : BaseCommand
+{
+    public UpdateUserAddressCommand(ConsoleKey triggerKey, IUserService userService)
+    : base(triggerKey, userService) { }
+
+
+    public override Task Execute(Guid? currentUserId)
+    {
+        throw new NotImplementedException();
+    }
+
+}
