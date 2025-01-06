36fd78adc784c3fe461ec1db3a34cfb660cc9b51
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-01-06 13:22:32 +0100

    Update

--- DatabaseService/EcommerceContext.cs
+++ DatabaseService/EcommerceContext.cs
@@ -1,136 +1,127 @@
-using System.Net.Sockets;
-using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace E_commerce_Databaser_i_ett_sammanhang;
-
-public class EcommerceContext : DbContext
+namespace E_commerce_Databaser_i_ett_sammanhang.DatabaseService
 {
-    public DbSet<User> Users { get; set; }
-    public DbSet<Product> Products { get; set; }
-    public DbSet<Order> Orders { get; set; }
-    public DbSet<OrderProduct> OrderProducts { get; set; }
-    public DbSet<Address> Addresses { get; set; }
-    // public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    using System.Net.Sockets;
+    using Microsoft.EntityFrameworkCore;
 
-    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    public class EcommerceContext : DbContext
     {
-        builder.UseNpgsql("Host=localhost;Database=ECommerce;Username=postgres;Password=password");
-    }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+        public DbSet<Address> Addresses { get; set; }
 
-    // Om man vill konfigurera modellerna med constraints:
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        builder.Entity<User>(user =>
+        // public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-            user.HasKey(u => u.UserId);
+            builder.UseNpgsql(
+                "Host=localhost;Database=ECommerce;Username=postgres;Password=password"
+            );
+        }
 
-            user.Property(u => u.FirstName)
-                .IsRequired()
-                .HasMaxLength(75);
-
-            user.Property(u => u.LastName)
-                .IsRequired()
-                .HasMaxLength(75);
-
-            user.Property(u => u.Email)
-                .IsRequired()
-                .HasMaxLength(100);
-
-            user.HasIndex(u => u.Email).IsUnique();
-
-            user.Property(u => u.PasswordHash)
-                .IsRequired()
-                .HasMaxLength(100);
-
-            user.Property(u => u.CreatedAt)
-                .IsRequired()
-                .HasDefaultValueSql("CURRENT_TIMESTAMP")
-                .HasColumnType("timestamp(0) with time zone");
-
-            // Relationship configuration
-            user.HasOne(u => u.Address)
-                .WithOne(a => a.User)
-                .HasForeignKey<Address>(a => a.UserId)
-                .IsRequired(false)
-                .OnDelete(DeleteBehavior.Cascade);
-        });
-
-        builder.Entity<Address>(address =>
+        // Om man vill konfigurera modellerna med constraints:
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            address.HasKey(a => a.AddressId);
+            builder.Entity<User>(user =>
+            {
+                user.HasKey(u => u.UserId);
 
-            address.Property(a => a.UserId)
-                .IsRequired(false);
+                user.Property(u => u.FirstName).IsRequired().HasMaxLength(75);
 
-            address.Property(a => a.Street)
-                .IsRequired()
-                .HasMaxLength(100);
+                user.Property(u => u.LastName).IsRequired().HasMaxLength(75);
 
-            address.Property(a => a.City)
-                .IsRequired()
-                .HasMaxLength(50);
+                user.Property(u => u.Email).IsRequired().HasMaxLength(100);
 
-            address.Property(a => a.Region)
-                .IsRequired()
-                .HasMaxLength(50);
+                user.HasIndex(u => u.Email).IsUnique();
 
-            address.Property(a => a.PostalCode)
-                .IsRequired()
-                .HasMaxLength(25);
+                user.Property(u => u.PasswordHash).IsRequired().HasMaxLength(100);
 
-            address.Property(a => a.Country)
-                .IsRequired()
-                .HasMaxLength(50);
-        });
+                user.Property(u => u.CreatedAt)
+                    .IsRequired()
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                    .HasColumnType("timestamp(0) with time zone");
 
-        builder.Entity<Order>(order =>
-        {
-            order.HasKey(o => o.OrderId);
+                // Relationship configuration
+                user.HasOne(u => u.Address)
+                    .WithOne(a => a.User)
+                    .HasForeignKey<Address>(a => a.UserId)
+                    .IsRequired(false)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
 
-            order.HasIndex(o => o.UserId);
+            builder.Entity<Address>(address =>
+            {
+                address.HasKey(a => a.AddressId);
 
-            order.Property(o => o.CreatedAt)
-                .IsRequired()
-                .HasDefaultValueSql("CURRENT_TIMESTAMP")
-                .HasColumnType("timestamp(0) with time zone");
+                address.Property(a => a.UserId).IsRequired(false);
 
-            order.Property(o => o.Status)
-                .IsRequired()
-                .HasConversion<string>()
-                .HasDefaultValueSql("Pending");
+                address.Property(a => a.Street).IsRequired().HasMaxLength(100);
 
-            order.Property(o => o.TotalCost)
-                .IsRequired()
-                .HasColumnType("decimal(10, 2)");
+                address.Property(a => a.City).IsRequired().HasMaxLength(50);
 
-            // Relationship configuration
-            order.HasOne(o => o.User)
-                .WithMany(u => u.Orders)
-                .HasForeignKey(o => o.UserId)
-                .OnDelete(DeleteBehavior.Cascade);
-        });
+                address.Property(a => a.Region).IsRequired().HasMaxLength(50);
 
+                address.Property(a => a.PostalCode).IsRequired().HasMaxLength(25);
 
-        builder.Entity<OrderProduct>(orderProduct =>
-        {
-            orderProduct.HasKey(op => new { op.OrderId, op.ProductId });
+                address.Property(a => a.Country).IsRequired().HasMaxLength(50);
+            });
 
-            orderProduct.HasIndex(op => op.ProductId);
+            builder.Entity<Order>(order =>
+            {
+                order.HasKey(o => o.OrderId);
 
-            orderProduct.Property(op => op.Quantity)
-                        .IsRequired();
+                order.HasIndex(o => o.UserId);
 
-            // Relationship with Order
-            orderProduct.HasOne(op => op.Order)
-                        .WithMany(o => o.OrderProducts)
-                        .HasForeignKey(op => op.OrderId)
-                        .OnDelete(DeleteBehavior.Cascade);
+                order
+                    .Property(o => o.CreatedAt)
+                    .IsRequired()
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                    .HasColumnType("timestamp(0) with time zone");
 
-            // Relationship with Product
-            orderProduct.HasOne(op => op.Product)
-                        .WithMany()
-                        .HasForeignKey(op => op.ProductId)
-                        .OnDelete(DeleteBehavior.Restrict);
-        });
+                order
+                    .Property(o => o.Status)
+                    .IsRequired()
+                    .HasConversion<string>()
+                    .HasDefaultValueSql("Pending");
+
+                order.Property(o => o.TotalCost).IsRequired().HasColumnType("decimal(10, 2)");
+
+                // Relationship configuration
+                order
+                    .HasOne(o => o.User)
+                    .WithMany(u => u.Orders)
+                    .HasForeignKey(o => o.UserId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            builder.Entity<OrderProduct>(orderProduct =>
+            {
+                orderProduct.HasKey(op => new { op.OrderId, op.ProductId });
+
+                orderProduct.HasIndex(op => op.ProductId);
+
+                orderProduct.Property(op => op.Quantity).IsRequired();
+
+                // Relationship with Order
+                orderProduct
+                    .HasOne(op => op.Order)
+                    .WithMany(o => o.OrderProducts)
+                    .HasForeignKey(op => op.OrderId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                // Relationship with Product
+                orderProduct
+                    .HasOne(op => op.Product)
+                    .WithMany()
+                    .HasForeignKey(op => op.ProductId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
     }
 }
