4137cf640257f28c3a6c631a4bef0d58202c01c5
alexanderluukkonens <alexanderluukkonens@hotmail.com>
2025-01-08 16:22:19 +0100

    worked on productcommand and ADD helper and saved

--- Commands/ProductCommand.cs
+++ Commands/ProductCommand.cs
@@ -47,7 +47,59 @@ public class ProductCommand : MenuBaseCommand
                 case ConsoleKey.D1:
                     List<List<Product>> allProducts = productMenu.EditContent(products);
 
+                    while (true)
+                    {
+                        productMenu.Display();
 
+                        var key = CustomKeyReader.GetKeyOrBuffered();
+
+                        if (key.Key == ConsoleKey.Escape)
+                        {
+                            break;
+                        }
+
+                        if (key.Key == ConsoleKey.LeftArrow || key.Key == ConsoleKey.RightArrow)
+                        {
+                            productMenu.SetPage(key.Key);
+                            index = productMenu.GetPage();
+                            continue;
+                        }
+
+                        currentPage = allProducts[index];
+
+                        string fullLine = CustomKeyReader.GetBufferedLine();
+                        if (!int.TryParse(fullLine, out int choice))
+                        {
+                            Utilities.WriteLineWithPause("You have to enter a number.");
+                            continue;
+                        }
+
+                        Product selectedProduct;
+                        if (choice > 0 && choice <= currentPage.Count)
+                        {
+                            selectedProduct = currentPage[choice - 1];
+                            productMenu.DisplayProduct(selectedProduct);
+                        }
+                        else
+                        {
+                            Utilities.WriteLineWithPause("Please select a product from the list.");
+                            continue;
+                        }
+
+                        ConsoleKey input2 = Console.ReadKey().Key;
+
+                        if (key.Equals(ConsoleKey.D1))
+                        {
+                            _shoppingCartService.AddToShoppingCart(selectedProduct.ProductId, selectedProduct.Price);
+                            break;
+                        }
+                        else if (key.Equals(ConsoleKey.Escape))
+                        {
+                            break;
+                        }
+                        Utilities.WriteLineWithPause("Incorrect input.");
+                        continue;
+                    }
                     break;
 
                 case ConsoleKey.D2:
