c9e855f6a652c4d58fc20efed44f114c4e7963a9
alexanderluukkonens <alexanderluukkonens@hotmail.com>
2025-01-09 10:18:53 +0100

    Add required to product-model

--- Product.cs
+++ Product.cs
@@ -3,12 +3,12 @@ namespace E_commerce_Databaser_i_ett_sammanhang;
 public class Product
 {
     public int ProductId { get; set; }
-    public string? Name { get; set; }
-    public string? Category { get; set; }
-    public string? Description { get; set; }
-    public decimal Price { get; set; }
-    public int Rating { get; set; }
-    public bool Available = false;
+    public required string Name { get; set; }
+    public required string Category { get; set; }
+    public required string Description { get; set; }
+    public required decimal Price { get; set; }
+    public required int Rating { get; set; }
+    public required bool Available = false;
     public OrderProduct OrderProduct { get; set; }
     public ShoppingCart? ShoppingCart { get; set; } // Build a relationsship
     // between OrderProduct and Product (Many-to-One)
