a2b52cb1e3de7f638db339d6421b4a33c8c5d828
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-01-09 14:01:08 +0100

    Created class for holding objects for cart, easier for how menu is designed.

---
+++ Models/CartItem.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_commerce_Databaser_i_ett_sammanhang;
+
+public class CartItem
+{
+    public string Name { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}
